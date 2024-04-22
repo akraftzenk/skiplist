b7245d38848bd2b2cf7f1dd7539e6b37de363bef
Kitzman <kitzmann@msoe.edu>
2024-04-22 13:35:10 -0500

    fixed class init as well as node class

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 class Node:
-    next = None
-    prev = None
-    value = None
-    def init(self, next, prev, value):
+    def __init__(self, next, prev, value):
         self.next = next
         self.prev = prev
         self.value = value
 
-class Skiplist:
 
-    def init(self, next, prev, value):
+class SkipList:
 
+    def __init__(self, list):
+        print("turn list into skiplist")
 
     @classmethod
     def add(self, element):
@@ -22,27 +20,32 @@ class Skiplist:
 
     @classmethod
     def get(self, element):
-        print("got..")
-
-
-
-
-
-def add_element(element):
-    print("add")
-
-def remove_element(element):
-    print("remove")
-
-def get_element(element):
-    print("get")
-
-def create_skiplist(list):
+        print("got")
 
 
 if __name__ == '__main__':
     list = []
+    node = Node(None, None, 1)
+    node2 = Node(None, node, 2)
+    node3 = Node(None, node2, 3)
+    node.next = node2
+    node2.next = node3
 
-    skiplist = create_skiplist(list)
+    print(node.value)
+    print(node.next.value)
+    print(node.next.next.value)
+    print("\n")
 
-    add_element()
+    print(node2.value)
+    print(node2.prev.value)
+    print(node2.next.value)
+    print("\n")
+
+    print(node3.value)
+    print(node3.prev.value)
+    print(node3.prev.prev.value)
+
+    skipList = SkipList([1,2,3])
+    skipList.add(1)
+    skipList.get(1)
+    skipList.remove(1)
