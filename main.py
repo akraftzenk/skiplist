fca9e30d941383f8ededa561ea50af90054b89dd
Kitzman <kitzmann@msoe.edu>
2024-04-22 11:52:30 -0500

    Inital method and class stubs

---
+++ main.py
@@ -0,0 +1,48 @@
+class Node:
+    next = None
+    prev = None
+    value = None
+    def init(self, next, prev, value):
+        self.next = next
+        self.prev = prev
+        self.value = value
+
+class Skiplist:
+
+    def init(self, next, prev, value):
+
+
+    @classmethod
+    def add(self, element):
+        print("add")
+
+    @classmethod
+    def remove(self, element):
+        print("remove")
+
+    @classmethod
+    def get(self, element):
+        print("got..")
+
+
+
+
+
+def add_element(element):
+    print("add")
+
+def remove_element(element):
+    print("remove")
+
+def get_element(element):
+    print("get")
+
+def create_skiplist(list):
+
+
+if __name__ == '__main__':
+    list = []
+
+    skiplist = create_skiplist(list)
+
+    add_element()
