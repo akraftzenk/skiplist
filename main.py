277a264f55b53accc36ba401a9b7518422052dce
Kitzman <kitzmann@msoe.edu>
2024-04-23 09:40:39 -0500

    added the search and add need to check to make sure it is in fact O log n

--- main.py
+++ main.py
@@ -1,51 +1,121 @@
+import random
+
+
 class Node:
-    def __init__(self, next, prev, value):
+    def __init__(self, next, prev, value, below, above):
         self.next = next
         self.prev = prev
         self.value = value
+        self.below = below
+        self.above = above
 
 
 class SkipList:
+    def __init__(self, list_to_convert):
+        self.height = 0
+        self.level_zero_head = Node(None, None, None, None, None)
+        self.levels = [self.level_zero_head]
+        for item in list_to_convert:
+            self.add(item)
 
-    def __init__(self, list):
-        print("turn list into skiplist")
-
-    @classmethod
     def add(self, element):
-        print("add")
+        pointer = self.levels[self.height]
+        level = self.height
+        while level > 0:
+            while pointer.next is not None and pointer.next.value < element:
+                pointer = pointer.next
+            level -= 1
+            pointer = pointer.below
 
-    @classmethod
+        while pointer.next is not None and pointer.next.value < element:
+            pointer = pointer.next
+        new_node = Node(pointer.next, pointer, element, None, None)
+        pointer.next = new_node
+
+        level = 0
+        last_level_node = new_node
+        while self.__coin_flip():
+            level += 1
+            if level > self.height:
+                new_level_head = Node(None, None, None, self.levels[self.height], None)
+                self.levels.append(new_level_head)
+                self.levels[self.height].above = new_level_head
+                self.height += 1
+
+                temp = Node(None, new_level_head, element, last_level_node, None)
+                last_level_node.above = temp
+                last_level_node = temp
+                new_level_head.next = last_level_node
+            else:
+                while pointer.above is None:
+                    pointer = pointer.prev
+                pointer = pointer.above
+                new_level_node = Node(pointer.next, pointer, element, last_level_node, None)
+                pointer.next = new_level_node
+                last_level_node.above = new_level_node
+                last_level_node = new_level_node
+
     def remove(self, element):
         print("remove")
 
-    @classmethod
     def get(self, element):
-        print("got")
+        pointer = self.levels[self.height]
+        level = self.height
+        while level > 0:
+            while pointer.next is not None and pointer.next.value < element:
+                pointer = pointer.next
+            level -= 1
+            pointer = pointer.below
+        while pointer.next is not None and pointer.next.value < element:
+            pointer = pointer.next
+        if pointer.next is None or pointer.next.value is not element:
+            return None
+        if pointer.next.value is element:
+            return pointer.next
+
+    def __coin_flip(self):
+        return random.choice([0, 1]) == 0
+
+    def print(self):
+        print("Total height: " + self.height.__str__())
+        for i in range(self.height + 1):
+            pointer = self.levels[i].next
+            print("h: " + i.__str__())
+            while pointer is not None:
+                print(pointer.value)
+                if i > 0:
+                    print("below : " + pointer.below.value.__str__())
+
+                if pointer.above is not None:
+                    print("above : " + pointer.above.value.__str__())
+                pointer = pointer.next
 
 
 if __name__ == '__main__':
-    list = []
-    node = Node(None, None, 1)
-    node2 = Node(None, node, 2)
-    node3 = Node(None, node2, 3)
-    node.next = node2
-    node2.next = node3
+    skipList = SkipList([1, 2, 3, 4, 5, 6, 7, 8, 10, 15])
 
-    print(node.value)
-    print(node.next.value)
-    print(node.next.next.value)
+    # print(skipList.print())
+    # skipList.add(1)
+    # result = skipList.get(9)
+    # if result is None:
+    #     print(result)
+    # else:
+    #     print(result.value)
+    #     print(result.prev.value)
+    #     print(result.next.value)
+    # skipList.remove(1)
+    # skipList.add(9)
+    # result = skipList.get(9)
+    # if result is None:
+    #     print(result)
+    # else:
+    #     print(result.value)
+    #     print(result.prev.value)
+    #     print(result.next.value)
+    #
+    skipList.print()
     print("\n")
-
-    print(node2.value)
-    print(node2.prev.value)
-    print(node2.next.value)
-    print("\n")
-
-    print(node3.value)
-    print(node3.prev.value)
-    print(node3.prev.prev.value)
-
-    skipList = SkipList([1,2,3])
-    skipList.add(1)
-    skipList.get(1)
-    skipList.remove(1)
+    skipList2 = SkipList([])
+    skipList2.add(10)
+    skipList2.add(0)
+    skipList2.print()
